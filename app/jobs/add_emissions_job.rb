cd7a4bf49cf02eed283a0a8c37a051b5b90b32f9
felipemrvieira <felipemrvieira@gmail.com>
2022-04-14 18:10:12 -0300

    fix emissions deletion

--- app/jobs/add_emissions_job.rb
+++ app/jobs/add_emissions_job.rb
@@ -4,10 +4,24 @@ class AddEmissionsJob
   include Sidekiq::Worker
   sidekiq_options retry: false
 
+  # territory_id is the id of the state
   def perform(csv_file, sector_id, territory_id)
-    # Delete all emissions per sector before insert new ones
-    Emission.delete_by(sector_id: sector_id)
+    # Delete all emissions per sector and territory before insert new ones
+   
+    last_emission_upload = EmissionUpload.where(
+      territory_id: territory_id, 
+      sector_id: sector_id,
+    ).last
     
+    emissions = last_emission_upload.emissions if last_emission_upload
+    emissions.delete_all if emissions
+
+    # EmissionUpload.emissions.delete_by(
+    #   sector_id: sector_id, 
+    #   territory_id: territory_id
+    # )
+    
+
     emission_upload = EmissionUpload.create(
       admin_id: 1,
       file_name: csv_file.split("/").last,
@@ -18,31 +32,39 @@ class AddEmissionsJob
     )
     
     emissions = []
+
+    emission_type_hash = Hash.new(:not_found)
+    EmissionType.all.each do |item|
+      # remove all spaces and convert to lowercase
+      emission_type_hash[item.name.gsub(/\s+/, "").downcase] = item
+    end
+   
+    gas_hash = Hash.new(:not_found)
+    Gas.all.each do |item|
+      # remove all spaces and convert to lowercase
+      gas_hash[item.name.gsub(/\s+/, "").downcase] = item
+    end
     
     CSV.foreach(csv_file, headers: true) do |emission|
       
       # Year range for city emissions
       for year_index in 1990..2019
-
-        puts emission.field(year_index.to_s) 
         
         emissions << {
-          year: year_index,
-          value: BigDecimal(emission.field(year_index.to_s).gsub(',', '.')),
-          emission_type_id: rand(1..8),
-          # territory_uf: emission.field("TERRITÓRIO"),
-          territory_id: 1,
-          sector_id: sector_id,
-          economic_activity_id: 1,
-          product_id: 1,
-          gas_id: 1,
-          # gas_id: emission.field("GÁS"),
-          level_2_id: 1,
-          level_3_id: 1,
-          level_4_id: 1,
-          level_5_id: 1,
-          level_6_id: 1,
-          emission_upload_id: emission_upload.id
+          year:                   year_index,
+          value:                  BigDecimal(emission.field(year_index.to_s).gsub(',', '.')),
+          emission_type_id:       emission_type_hash[emission.field("TIPO DE EMISSAO").gsub(/\s+/, "").downcase].id,
+          sector_id:              sector_id,
+          gas_id:                 gas_hash[emission.field("GÁS").gsub(/\s+/, "").downcase].id,
+          emission_upload_id:     emission_upload.id,
+          territory_id:           1,
+          economic_activity_id:   1,
+          product_id:             1,
+          level_2_id:             1,
+          level_3_id:             1,
+          level_4_id:             1,
+          level_5_id:             1,
+          level_6_id:             1
         }
 
       end
@@ -52,9 +74,7 @@ class AddEmissionsJob
     result = Emission.insert_all(emissions)
     puts "Emissions Total After --------->>>>>>>>>> #{Emission.count}"
     
-    # puts result.inspect
-    puts sector_id
-    
+    # puts result.inspect    
   end
   
 end
--- app/models/emission_upload.rb
+++ app/models/emission_upload.rb
@@ -2,5 +2,5 @@ class EmissionUpload < ApplicationRecord
   belongs_to :admin
   belongs_to :sector
   belongs_to :territory
-  has_many :emissions
+  has_many :emissions, dependent: :delete_all
 end
